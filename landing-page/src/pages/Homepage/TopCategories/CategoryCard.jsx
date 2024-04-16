73426eded280385dbed25666f8d3490ab8b62af6
skowalczyk <skowalczyk@griddynamics.com>
2024-04-16 16:28:25 +0200

    CategoryCard hover update

--- landing-page/src/pages/Homepage/TopCategories/CategoryCard.jsx
+++ landing-page/src/pages/Homepage/TopCategories/CategoryCard.jsx
@@ -9,10 +9,11 @@ function CategoryCard({ product }) {
   return (
     <article className="group flex flex-col items-center gap-8 py-12 transition-all hover:translate-y-[-24px]">
       <div className="relative h-[272px] w-[272px] rounded-full">
+        <div className="bg-grey-3 absolute -z-10 h-[268px] w-[268px] translate-x-[1px] translate-y-[1px] rounded-full"></div>
         <img
           src={product.thumbnail}
           alt={product.name}
-          className="h-full w-full rounded-full object-cover transition-all group-hover:shadow-2xl"
+          className="h-full w-full rounded-full object-cover transition-all group-hover:-translate-y-2 group-hover:translate-x-2 group-hover:shadow-2xl"
         />
         <div className="opacity-0 transition-all group-hover:opacity-100">
           <ButtonGreen style={{ bottom: "33px" }}>View Details</ButtonGreen>
