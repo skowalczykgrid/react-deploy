ca82168515bd79f61e1f6a7df16285805e3c1040
skowalczyk <skowalczyk@griddynamics.com>
2024-04-17 17:52:02 +0200

    clicking logo jumps to top of the homepage

--- landing-page/src/components/Logo.jsx
+++ landing-page/src/components/Logo.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
 import logoImg from "../assets/Logo.svg";
 
 function Logo({ className }) {
-  //   return <span className="text-textPrimary text-[35px] font-bold">Hekto</span>;
-  return <img src={logoImg} alt="logo" className={className} />;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  };
+
+  return (
+    <Link to="/" onClick={scrollToTop}>
+      <img src={logoImg} alt="logo" className={className} />
+    </Link>
+  );
 }
 
 export default Logo;
